90c70fbbfd66b3f2a1a07bce5e983765c56c2455
happyytw <happyytw@gmail.com>
2024-07-07 22:10:45 +0900

    create project

---
+++ SmartExpenceTracker/SmartExpenceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartExpenceTrackerApp.swift
+//  SmartExpenceTracker
+//
+//  Created by Taewon Yoon on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartExpenceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
