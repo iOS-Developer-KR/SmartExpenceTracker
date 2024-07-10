afc011c47bd6000b12ad58071314343d50bed3e9
happyytw <happyytw@gmail.com>
2024-07-10 21:45:29 +0900

    change navigation to sheet

--- SmartExpenceTracker/GPT.swift
+++ SmartExpenceTracker/GPT.swift
@@ -12,7 +12,7 @@ import Combine
 
 struct Receipts: Codable {
     var title: String
-    var amount: String
+    var amount: Int
     var category: String
     var date: String
 }
@@ -20,7 +20,7 @@ struct Receipts: Codable {
 
 class GPT: ObservableObject {
     
-    @Published var result: Receipts = Receipts(title: "", amount: "0$", category: "no value", date: "")
+    @Published var result: Receipts = Receipts(title: "no value", amount: 0, category: "no value", date: "no value")
     @Published var navigate: Bool = false
     var openAI = OpenAI(apiToken: "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")
     var cancellables = Set<AnyCancellable>()
@@ -29,10 +29,6 @@ class GPT: ObservableObject {
         Task {
             await addSubscriber()
         }
-    }
-    
-    func reset() {
-        result = .init(title: "", amount: "", category: "", date: "")
     }
     
     @MainActor
@@ -45,61 +41,43 @@ class GPT: ObservableObject {
                         .init(
                             type: .object,
                             properties: [
-                                "title": .init(type: .string, description: "Set the title of this reciepts"),
-                                "amount": .init(type: .string, description: "total payment including currency unit. If you're not sure, just use USD as default value"),
+                                "title": .init(type: .string, description: "Set the simple title of this reciepts"),
+                                "amount": .init(type: .integer, description: "total payment. If you're not sure, just use USD as default value"),
                                 "category": .init(type: .string, description: "the category of expense. Set category based on the title of expense"),
                                 "date": .init(type: .string, description: "Date of expense. Set date using of this reciepts information, the date must be formatted yyy-MM-dd")
                             ],
                             required: ["title", "amount", "category"]
                         )
-            )),
-            //            ChatQuery.ChatCompletionToolParam(function: .init(
-            //                name: "Analysing-Given-Reciepts",
-            //                description: "Analyze this Reciepts by given image",
-            //                parameters:
-            //                        .init(
-            //                            type: .array,
-            //                            properties: [
-            //                                "object": .init(type: .string, description: "the name of object"),
-            //                                "amount": .init(type: .string, description: "payment for object. this include current unit If you're not sure, just use USD as default value")
-            //                            ],
-            //                            required: ["title", "amount", "category"]
-            //                        )
-            //                )
-            //             )
-            
+                )
+             )
         ]
         
         print("analysing2")
         let imageParam = ChatQuery.ChatCompletionMessageParam.ChatCompletionUserMessageParam.init(
             content:
-                    .vision([
-                        .chatCompletionContentPartImageParam(.init(imageUrl: .init(url: imageData, detail: .high)))
-                    ])
+                .vision([
+                    .chatCompletionContentPartImageParam(.init(imageUrl: .init(url: imageData, detail: .high)))
+                ])
         )
         
         Task {
-            let chatsStream = try await openAI.chats(query: ChatQuery(messages: [.user(imageParam)], model: .gpt4_o, tools: functions))
-            for chat in chatsStream.choices {
-                for tool in chat.message.toolCalls! {
-                    print(tool.function.arguments.description)
-                }
-                if let arg = chat.message.toolCalls?.first?.function.arguments.data(using: .utf8) {
-                    DispatchQueue.main.async { [self] in
-                        do {
-                            
-                            self.result = try JSONDecoder().decode(Receipts.self, from: arg)
+            do {
+                let chatsStream = try await openAI.chats(query: ChatQuery(messages: [.user(imageParam)], model: .gpt4_o, tools: functions))
+                for chat in chatsStream.choices {
+                    print(chat.message.toolCalls?.first?.function.arguments.description)
+                    if let arg = chat.message.toolCalls?.first?.function.arguments.data(using: .utf8) {
+                        DispatchQueue.main.async { [self] in
+                            self.result = try! JSONDecoder().decode(Receipts.self, from: arg)
                             print("해독한 json" + self.result.category + self.result.date + result.title)
-                        } catch {
-                            print(error.localizedDescription)
                         }
+                    } else {
+                        print("no data")
                     }
-                } else {
-                    print("no data")
                 }
+                print("")
+            } catch {
+                print(error.localizedDescription)
             }
-            print("")
-            
         }
     }
     
@@ -107,20 +85,20 @@ class GPT: ObservableObject {
     func addSubscriber() {
         $result
             .sink { [weak self] receipts in
-//                if receipts.title.isEmpty {
-//                    self?.navigate = false // 만약 비어있지 않다면
-//                } else {
-                    self?.navigate = true // 만약 비어있다면
-//                }
+                if receipts.amount == 0 {
+                    self?.navigate = false
+                } else {
+                    self?.navigate = true
+                }
             }
             .store(in: &cancellables)
     }
-    
+
 }
 
 
 //extension Binding where Value == Bool {
-//
+//    
 //    init<T>(value: Binding<T?>) {
 //        self.init {
 //            value.wrappedValue != nil
@@ -134,49 +112,49 @@ class GPT: ObservableObject {
 
 /*
  func gptCall() async {
- let functions = [
- ChatQuery.ChatCompletionToolParam(function: .init(
- name: "Analysing-Given-Reciepts",
- description: "Analyze this Reciepts by given image",
- parameters:
- .init(
- type: .object,
- properties: [
- "title": .init(type: .string, description: "Set the title of this reciepts"),
- "amount": .init(type: .integer, description: "total payment. If you're not sure, just use USD as default value"),
- "category": .init(type: .string, description: "the category of expense. Set category based on the title of expense"),
- "date": .init(type: .string, description: "Date of expense. Set date using of this reciepts information, the date must be formatted yyy-MM-dd")
- ],
- required: ["title", "amount", "category"]
- )
- )
- )
- ]
- 
- if let image = UIImage(named: "japan1"), let imageData = image.jpegData(compressionQuality: 1.0) {
- let imageParam = ChatQuery.ChatCompletionMessageParam.ChatCompletionUserMessageParam.init(
- content:
- .vision([
- .chatCompletionContentPartImageParam(.init(imageUrl: .init(url: imageData, detail: .high)))
- ])
- )
- 
- do {
- let chatsStream = try await openAI.chats(query: ChatQuery(messages: [.user(imageParam)], model: .gpt4_o, tools: functions))
- for chat in chatsStream.choices {
- if let arg = chat.message.toolCalls?.first?.function.arguments.data(using: .utf8), let decoded = try? JSONDecoder().decode(Receipts.self, from: arg) {
- print("해독한 json" + decoded.category + decoded.date + decoded.title)
- }
- 
- }
- } catch {
- print(error.localizedDescription)
- }
- 
- 
- 
- } else {
- print("else문")
- }
+     let functions = [
+         ChatQuery.ChatCompletionToolParam(function: .init(
+             name: "Analysing-Given-Reciepts",
+             description: "Analyze this Reciepts by given image",
+             parameters:
+                     .init(
+                         type: .object,
+                         properties: [
+                             "title": .init(type: .string, description: "Set the title of this reciepts"),
+                             "amount": .init(type: .integer, description: "total payment. If you're not sure, just use USD as default value"),
+                             "category": .init(type: .string, description: "the category of expense. Set category based on the title of expense"),
+                             "date": .init(type: .string, description: "Date of expense. Set date using of this reciepts information, the date must be formatted yyy-MM-dd")
+                         ],
+                         required: ["title", "amount", "category"]
+                     )
+             )
+          )
+     ]
+
+     if let image = UIImage(named: "japan1"), let imageData = image.jpegData(compressionQuality: 1.0) {
+         let imageParam = ChatQuery.ChatCompletionMessageParam.ChatCompletionUserMessageParam.init(
+             content:
+                 .vision([
+                     .chatCompletionContentPartImageParam(.init(imageUrl: .init(url: imageData, detail: .high)))
+                 ])
+         )
+         
+         do {
+             let chatsStream = try await openAI.chats(query: ChatQuery(messages: [.user(imageParam)], model: .gpt4_o, tools: functions))
+             for chat in chatsStream.choices {
+                 if let arg = chat.message.toolCalls?.first?.function.arguments.data(using: .utf8), let decoded = try? JSONDecoder().decode(Receipts.self, from: arg) {
+                     print("해독한 json" + decoded.category + decoded.date + decoded.title)
+                 }
+                 
+             }
+         } catch {
+             print(error.localizedDescription)
+         }
+         
+
+
+     } else {
+         print("else문")
+     }
  }
  */
--- SmartExpenceTracker/SavingExpenceView.swift
+++ SmartExpenceTracker/SavingExpenceView.swift
@@ -65,3 +65,10 @@ struct SavingExpenceView: View {
 }
 
 
+
+func convertToDate(from string: String) -> Date? {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd" // 문자열의 형식을 지정
+    return formatter.date(from: string)
+}
+
