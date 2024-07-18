56020188e88638c151c9b2191086e0d5da788ee9
happyytw <happyytw@gmail.com>
2024-07-18 21:55:14 +0900

    clean code

--- SmartExpenceTracker/GPT.swift
+++ SmartExpenceTracker/GPT.swift
@@ -54,7 +54,7 @@ class GPT: ObservableObject {
     
     init() {
         Task {
-            await addSubscriber()
+            addSubscriber()
         }
     }
     
@@ -137,9 +137,7 @@ class GPT: ObservableObject {
                     }
                     
                     //marchants
-                    for tool in chat.message.toolCalls! {
-                        
-                    }
+
                 }
                 print("")
             } catch {
@@ -163,66 +161,3 @@ class GPT: ObservableObject {
     }
 
 }
-
-
-//extension Binding where Value == Bool {
-//    
-//    init<T>(value: Binding<T?>) {
-//        self.init {
-//            value.wrappedValue != nil
-//        } set: { newValue in
-//            if !newValue {
-//                value.wrappedValue = nil
-//            }
-//        }
-//    }
-//}
-
-/*
- func gptCall() async {
-     let functions = [
-         ChatQuery.ChatCompletionToolParam(function: .init(
-             name: "Analysing-Given-Reciepts",
-             description: "Analyze this Reciepts by given image",
-             parameters:
-                     .init(
-                         type: .object,
-                         properties: [
-                             "title": .init(type: .string, description: "Set the title of this reciepts"),
-                             "amount": .init(type: .integer, description: "total payment. If you're not sure, just use USD as default value"),
-                             "category": .init(type: .string, description: "the category of expense. Set category based on the title of expense"),
-                             "date": .init(type: .string, description: "Date of expense. Set date using of this reciepts information, the date must be formatted yyy-MM-dd")
-                         ],
-                         required: ["title", "amount", "category"]
-                     )
-             )
-          )
-     ]
-
-     if let image = UIImage(named: "japan1"), let imageData = image.jpegData(compressionQuality: 1.0) {
-         let imageParam = ChatQuery.ChatCompletionMessageParam.ChatCompletionUserMessageParam.init(
-             content:
-                 .vision([
-                     .chatCompletionContentPartImageParam(.init(imageUrl: .init(url: imageData, detail: .high)))
-                 ])
-         )
-         
-         do {
-             let chatsStream = try await openAI.chats(query: ChatQuery(messages: [.user(imageParam)], model: .gpt4_o, tools: functions))
-             for chat in chatsStream.choices {
-                 if let arg = chat.message.toolCalls?.first?.function.arguments.data(using: .utf8), let decoded = try? JSONDecoder().decode(Receipts.self, from: arg) {
-                     print("해독한 json" + decoded.category + decoded.date + decoded.title)
-                 }
-                 
-             }
-         } catch {
-             print(error.localizedDescription)
-         }
-         
-
-
-     } else {
-         print("else문")
-     }
- }
- */
