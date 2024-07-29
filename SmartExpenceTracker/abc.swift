222151927dae53a592334468013fac281101b7ba
happyytw <happyytw@gmail.com>
2024-07-29 21:55:29 +0900

    adding accessCameraView

--- SmartExpenceTracker/abc.swift
+++ SmartExpenceTracker/abc.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct abc: View {
     @State private var isOpaque = false
@@ -13,33 +14,7 @@ struct abc: View {
 
     var body: some View {
         ZStack {
-            GeometryReader(content: { geometry in
-                Image(uiImage: UIImage(named: "japan1")!)
-                    .resizable()
-                
-                Rectangle()
-                    .fill(Color.green)
-                    .frame(width: 400, height: 5)
-                    .opacity(isOpaque ? 0.3 : 0.8)
-                    .offset(y: isOffset ? 0 : (UIScreen.current?.bounds.height)! - 80)
-                    .overlay(
-                        Rectangle()
-                            .fill(Color.green)
-                            .frame(width: 400, height: 50)
-                            .opacity(isOpaque ? 0.3 : 0.8)
-                            .offset(y: isOffset ? 0 : (UIScreen.current?.bounds.height)! - 100)
-                    )
-                    .onAppear {
-                        withAnimation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
-                            isOpaque.toggle()
-                        }
-                        withAnimation(Animation.easeInOut(duration: 3).repeatForever(autoreverses: true)) {
-                            isOffset.toggle()
-                        }
-                    }
-                    
 
-            })
             
             
         }
